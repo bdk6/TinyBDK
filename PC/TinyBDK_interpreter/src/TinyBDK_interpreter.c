bd0a33697059ba45d19d1a5e744cef3a94432584
William R Cooke <wrcooke@wrcooke.net>
2022-03-11 07:26:58 -0600

    First code checkin

---
+++ PC/TinyBDK_interpreter/src/TinyBDK_interpreter.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TinyBDK_interpreter.c
+ Author      : Will Cooke
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
